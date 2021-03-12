923bd6b43aad3976fb50a1e1c1418617a5b5f673
Niv200 <Nivm200@gmail.com>
2021-03-12 20:12:21 +0200

    .

--- models/user.js
+++ models/user.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
-
+ 
 const UserSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+  },
+  count: {
+    type: Number,
+  },
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
 });
-
+ 
 module.exports = mongoose.model("Users", UserSchema);
--- server.js
+++ server.js
@@ -1,109 +1,195 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-
+require("dotenv").config();
 const User = require("./models/user");
-const Exercise = require("./models/exercise");
-const { response } = require('express');
-
+// const Exercise = require("./models/exercise");
+const { json } = require("body-parser");
+const { find } = require("./models/user");
+ 
+//----------------------------------middleware-----------------------------------------------
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
+ 
+//connecting to database
 mongoose
-  .connect(
-    "mongodb+srv://niv-database01:nivsdata@cluster0.cdi79.mongodb.net/exerciseTracker?retryWrites=true&w=majority",
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => console.log("Connected Successfully to Mongoose atlas"));
-
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    .connect(
+        "mongodb+srv://niv-database01:nivsdata@cluster0.cdi79.mongodb.net/exerciseTracker?retryWrites=true&w=majority",
+        {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => console.log("Connected Successfully to Mongoose atlas"))
+    .catch((err) => console.log(err));
+ 
+//-------------------------------------Routes---------------------------------------------
+ 
+//loading static HTML page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
-
+ 
+//Making a new user
 app.post("/api/exercise/new-user", async (req, res) => {
-  const body = req.body;
-  const userName = body.username;
-  const user = await User.find({ username: userName });
-  if (user[0] === undefined) {
-    const newUser = new User({
-      username: userName,
+    const body = req.body;
+    const userName = body.username;
+    if (userName === "") {
+        return res.status(400).send("Username cannot be empty");
+    }
+    let user;
+    try {
+        user = await User.find({ username: userName });
+    } catch (err) {
+        return res
+            .status(500)
+            .send("Internal server error could not find user" + err);
+    }
+    if (user[0] === undefined) {
+        const newUser = new User({
+            username: userName,
+            count: 0,
+        });
+        try {
+            await newUser.save();
+        } catch (err) {
+            return res.status(500).send("Internal server error" + err);
+        }
+        const id = newUser._id;
+        const username = newUser.username;
+        const obj = {
+            username: username,
+            _id: id,
+        };
+        console.log(obj.username);
+        res.json(obj);
+    } else {
+        return res.status(400).send("Username already taken");
+    }
+});
+ 
+//Get all users
+app.get("/api/exercise/users", async (req, res) => {
+    let userArray;
+    try {
+        userArray = await User.find({});
+    } catch (err) {
+        return res
+            .status(500)
+            .send("Internal server error, could not get users" + err);
+    }
+    res.json(userArray);
+});
+ 
+//Adding a new exercise to a user
+app.post("/api/exercise/add", async (req, res) => {
+    const body = req.body;
+    const exercise = {};
+    const outputObject = {};
+    if (body.date === "") {
+        exercise.date = new Date();
+    } else {
+        exercise.date = new Date(body.date);
+    }
+    exercise.duration = body.duration;
+    exercise.description = body.description;
+    try {
+        await User.findByIdAndUpdate(body.userId, {
+            $push: { log: exercise },
+            $inc: { count: 1 },
+        });
+    } catch (err) {
+        return res.status(500).send("Internal server error");
+    }
+    let user;
+    try {
+        user = await User.findById(body.userId);
+    } catch (err) {
+        return res
+            .status(500)
+            .send("Internal server error, could not find user" + err);
+    }
+    const id = user._id;
+    outputObject.username = user.username;
+    outputObject.description = exercise.description;
+    outputObject.duration = Number(exercise.duration);
+    outputObject._id = mongoose.Types.ObjectId(id);
+    outputObject.date = exercise.date.toDateString();
+    res.json(outputObject);
+});
+ 
+//Getting the user and all his exercises
+app.get("/api/exercise/log", async (req, res) => {
+    const query = req.query;
+    let user;
+    try {
+        user = await User.findById(query.userId);
+    } catch (err) {
+        return res.status(500).send("Internal server error");
+    }
+    const { log } = user;
+    let filtered;
+    filtered = log;
+ 
+    filtered.sort(function (a, b) {
+        let dateA = a.date,
+            dateB = b.date;
+        return dateA - dateB;
     });
-    await newUser.save();
-    const id = newUser._id;
-    const obj = {
-      username: newUser.username,
-      _id: id,
+ 
+    if (!query.userId) {
+        return res.status(400).send("userId required");
+    }
+ 
+    if (query.to) {
+        filtered = filtered.filter((element) => {
+            if (element.date <= new Date(query.to)) {
+                return element;
+            }
+        });
+    }
+    if (query.from) {
+        filtered = filtered.filter((element) => {
+            if (element.date >= new Date(query.from)) {
+                return element;
+            }
+        });
+    }
+ 
+    if (query.limit) {
+        query.limit =
+            query.limit > filtered.length ? filtered.length : query.limit;
+        const temp = [];
+        for (let i = 0; i < Number(query.limit); i++) {
+            temp.push(filtered[i]);
+        }
+        filtered = temp;
+    }
+    const temp = [];
+    for (const item of filtered) {
+        temp.push({
+            description: item.description,
+            duration: item.duration,
+            date: item.date.toDateString(),
+        });
+    }
+    filtered = temp;
+    const object = {
+        _id: mongoose.Types.ObjectId(user._id),
+        username: user.username,
+        count: filtered.length,
+        log: filtered,
     };
-    res.json(obj);
-  } else {
-    res.status(400).send("Username already taken");
-  }
+    return res.status(200).json(object);
 });
-
-app.post("/api/exercise/add", async (req, res) =>{
-  const body = req.body;
-  console.log(body);
-  let obj = {
-    description: body.description,
-    duration: parseInt(body.duration),
-    date: body.date
-  };
-  if(obj.date === ''){
-    obj.date = new Date().toISOString().substring(0, 10);
-  }
-  let exercise = new Exercise(obj);
-
-  const user = await User.findByIdAndUpdate(body.id,
-    {$push : {log: exercise}},
-    {new: true}, //Meaning itll return the new exercise and not the old.
-    (error, newUser) =>{
-
-    });
-  res.json(exercise);
-});
-
-// app.post("/api/exercise/add", async (req, res) =>{
-//   const body = req.body;
-//   let exercise;
-//   if(!body.date){
-//      exercise = new Exercise({
-//     userId: body.userId,
-//     description: body.description,
-//     duration: body.duration,
-//   });
-//   }else{
-//    exercise = new Exercise({
-//     userId: body.userId,
-//     description: body.description,
-//     duration: body.duration,
-//     date: body.date
-//   });
-// }
-//   await exercise.save();
-//   const obj = {};
-//   const id = exercise._id;
-//   const {username} = await Exercise.findById(id);
-//   obj._id = id;
-//   obj.username = username;
-//   obj.date = exercise.date;
-//   obj.duration = exercise.duration;
-//   res.json(obj);
-// });
-
-app.get("/api/exercise/users", async (req, res) =>{
-const userArray = await User.find({});
-res.json(userArray);
-});
-
-
+ 
+//------------------------------App-Listen---------------------------------------------
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    console.log("Your app is listening on port " + listener.address().port);
+});
